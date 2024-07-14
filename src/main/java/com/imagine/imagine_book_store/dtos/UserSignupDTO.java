1bf79192113d44594e1f21f40fe407c36424f559
Ahmed Alkaf <ahmedalkaff@yahoo.com>
2024-07-14 12:19:45 +0300

    Added simple validation for user signup dto

--- src/main/java/com/imagine/imagine_book_store/dtos/UserSignupDTO.java
+++ src/main/java/com/imagine/imagine_book_store/dtos/UserSignupDTO.java
@@ -1,5 +1,8 @@
 package com.imagine.imagine_book_store.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotBlank;
 
-public record UserSignupDTO(String name, String email, String password) {
+public record UserSignupDTO(@NotBlank String name, @NotBlank @Email String email, @NotBlank @Size(min = 8) String password) {
 }
--- src/main/java/com/imagine/imagine_book_store/exception/ExceptionAdvice.java
+++ src/main/java/com/imagine/imagine_book_store/exception/ExceptionAdvice.java
@@ -1,8 +1,11 @@
 package com.imagine.imagine_book_store.exception;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,12 +19,22 @@ public class ExceptionAdvice {
     return Map.of("error", ex.getMessage());
   }
 
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  Map<String, String> handleArgumentNotValid(MethodArgumentNotValidException ex){
+    Map<String, String> errors = new HashMap<>();
+    ex.getBindingResult().getAllErrors().forEach((error) -> {
+        String fieldName = ((FieldError) error).getField();
+        String errorMessage = error.getDefaultMessage();
+        errors.put(fieldName, errorMessage);
+    });
+    return errors;
+  }
+
   @ExceptionHandler(Exception.class)
   @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
   Map<String, String> handleException(Exception ex){
-    return Map.of("error", "internal server error"); 
+    return Map.of("error", "internal server error");
   }
-
-
 
 }
